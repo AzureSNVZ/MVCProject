1243d9c11a5b28250df716c24433a5685f7cd7a2
KiranSonawane2003 <Kiransonawane.nmims@gmail.com>
2018-09-27 15:38:25 +0530

    Simple web app mvc

---
+++ MVCProject/Controllers/ItemController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+  
+        public ViewResult Index()
+        {
+            ViewBag.ItemList = "Computer Shop Item List Page";
+            return View();
+        }
+        /*public EmptyResult Index()
+        {
+            ViewBag.ItemList = "Computer Shop Item List Page";
+            return new EmptyResult();
+        }
+        public RedirectResult Index()
+        {
+            return Redirect("Home/Contact");
+        }*/
+    }
+}
