e8577c657c758b08e9c0882dbfc1f80e20319f66
KiranSonawane2003 <37357051+KiranSonawane2003@users.noreply.github.com>
2018-10-12 14:21:08 +0530

    PowerShell Script
    .ps1

--- Backup.bat
+++ Backup.bat
@@ -1,1 +1,10 @@
-xcopy C:\inetpub\wwwroot\MVC C:\Backup /E /H
+$x="D:\Backup"
+$Z=Test-Path $x
+$dts="D:\Backup\Publish$((Get-Date).ToString('yyyy-MM-dd'))"
+
+if ($Z -ne $true){New-Item -ItemType Directory  -Path "D:\Backup"
+
+}
+
+New-Item -ItemType Directory -Path "D:\Backup\Publish$((Get-Date).ToString('yyyy-MM-dd'))"
+Copy-Item 'C:\inetpub\wwwroot\MVCJenkins' -Destination $dts -Force -Recurse -Verbose
